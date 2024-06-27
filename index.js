e947c04cc62e7bb4a9c4851c18e791c141e3b299
vinay kumar <kumarvinay07318@gmail.com>
2024-06-27 11:13:20 +0530

    hlw

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ const server =  app.listen(port,()=>{
 
 const io =  new Server(server,{
     cors:{
-        origin:`http://localhost:5173`,
+        origin:`https://chat-app-fronted-2.vercel.app`,
         credentials:true
     }
 });
